89c29c72363e0e1a22301c755141c764469f0639
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-20 03:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var x = [];
+  for (var i = 0; i < musicians.length; i++) {
+    x.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return x
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  while(i < array.length){
+    array[i] += '!!!'; i++;
+  }
+  return array
+}
+
+function iLoveTheBeatles()
